f38416287d8a6c85aa84932973ee4de1fd809142
natsu2912 <49586265+natsu2912@users.noreply.github.com>
2019-07-07 12:23:06 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,12 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install gdb -y
+sudo apt install gcc -y
+sudo apt install vim -y
+sudo apt install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential -y
+sudo pip install --upgrade pip
+sudo pip install --upgrade pwntools
+ 
+sudo apt install net-tools -y
+
+#Install peda, one_gadget, ROPGadget later
